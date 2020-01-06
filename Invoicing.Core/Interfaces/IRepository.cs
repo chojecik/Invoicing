bee9d630cfba5b5459063a94eb0db83876267dcd
Paweł Chojęta <chojecik@gmail.com>
2020-01-06 15:55:43 +0100

    add IRepository

---
+++ Invoicing.Core/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoicing.Core.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        ValueTask<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        Task UpdateAsync(TEntity entity);
+        Task UpdateRangeAsync(IEnumerable<TEntity> entities);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
