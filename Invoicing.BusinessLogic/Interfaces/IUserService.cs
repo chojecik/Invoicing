45332fe649ab6bb25a4b4f94a89e3140dfe3f6cd
Paweł Chojęta <chojecik@gmail.com>
2020-01-25 01:33:21 +0100

    add XML documentation comments for IService and IUserService

--- Invoicing.BusinessLogic/Interfaces/IUserService.cs
+++ Invoicing.BusinessLogic/Interfaces/IUserService.cs
@@ -2,9 +2,25 @@
 
 namespace Invoicing.BusinessLogic.Interfaces
 {
+    /// <summary>
+    /// Interface containing methods for User class
+    /// </summary>
     public interface IUserService : IService<User>
     {
+        /// <summary>
+        /// Inserts new user to the database
+        /// </summary>
+        /// <param name="user">Entity to insert</param>
+        /// <param name="password">User's password</param>
+        /// <returns>Returns created user</returns>
         User CreateUser(User user, string password);
+
+        /// <summary>
+        /// Authenticates the user
+        /// </summary>
+        /// <param name="email">User's email</param>
+        /// <param name="password">User's password</param>
+        /// <returns>Returns authenticated user</returns>
         User Authenticate(string email, string password);
     }
 }
