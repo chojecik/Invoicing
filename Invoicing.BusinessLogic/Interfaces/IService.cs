70e231e2314ccefbf898e23d34c02077a92e59b7
Paweł Chojęta <chojecik@gmail.com>
2020-01-25 01:36:05 +0100

    Change name of generic type from TEntity to T

--- Invoicing.BusinessLogic/Interfaces/IService.cs
+++ Invoicing.BusinessLogic/Interfaces/IService.cs
@@ -7,47 +7,47 @@ namespace Invoicing.BusinessLogic.Interfaces
     /// <summary>
     /// Interface containing basic methods for CRUD operations
     /// </summary>
-    /// <typeparam name="TEntity"></typeparam>
-    public interface IService<TEntity> where TEntity : class
+    /// <typeparam name="T"></typeparam>
+    public interface IService<T> where T : class
     {
         /// <summary>
         /// Gets all records of T type from the database
         /// </summary>
         /// <returns>Returns the IEmumerable collection of T type</returns>
-        IEnumerable<TEntity> GetAll();
+        IEnumerable<T> GetAll();
 
         /// <summary>
         /// Gets the record from the database by it's ID
         /// </summary>
         /// <param name="id">ID of the record</param>
         /// <returns>Returns single object of T type</returns>
-        TEntity GetById(int id);
+        T GetById(int id);
 
         /// <summary>
         /// Gets records of type T from the database by provided condition
         /// </summary>
         /// <param name="predicate">Condition</param>
         /// <returns>Returns IEnumerabl collection of T type</returns>
-        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
 
         /// <summary>
         /// Inserts the entity to the database
         /// </summary>
         /// <param name="entity">Entity to insert</param>
         /// <returns>Returns created entity of T type</returns>
-        TEntity Create(TEntity entity);
+        T Create(T entity);
 
         /// <summary>
         /// Updates the record in the database
         /// </summary>
         /// <param name="entity">Record to update</param>
         /// <returns>Returns updated entity of T type</returns>
-        TEntity Update(TEntity entity);
+        T Update(T entity);
 
         /// <summary>
         /// Deletes the record from the database
         /// </summary>
         /// <param name="entity">Entity to delete</param>
-        void Delete(TEntity entity);
+        void Delete(T entity);
     }
 }
